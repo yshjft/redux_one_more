73b799dcec929e27fa48ac0515e1814bf7facb47
hong yuseok <jericoh@naver.com>
2020-10-10 03:14:41 +0900

    리덕스 강의 듣기

--- vanila-todo/README.md
+++ vanila-todo/README.md
@@ -1,2 +1,7 @@
 vanila todo
 ===============
+
+### Never mutate state
+리덕스에서 state를 변경하고 싶은 경우 state를 새로 생성하고 새로운 state를 return 할 것          
+          
+           
--- vanila-todo/src/index.js
+++ vanila-todo/src/index.js
@@ -6,10 +6,26 @@ const ul = document.querySelector("ul");
 const ADD_TODO ="ADD_TODO"
 const DELETE_TODO ="DELETE_TODO"
 
+const addTodo = (text) =>{
+    return {
+        type : ADD_TODO,
+        text 
+    };
+};
+
+const deleteTodo = (id) =>{
+    return {
+        type : DELETE_TODO,
+        id
+    };
+};
+
+
+
 const reducer = (state=[], action) => {
     switch(action.type){
         case ADD_TODO:
-            return [];
+            return [...state, {text : action.text, id : Date.now()}];
         case DELETE_TODO:
             return [];
         default:
@@ -19,12 +35,37 @@ const reducer = (state=[], action) => {
 
 const store = createStore(reducer);
 
+const dispatchAddTodo = (text) =>{
+    store.dispatch(addTodo(text))
+}
+
+const dispatchDeleteTodo = (e) =>{
+    const id  = e.target.parentNode.id;
+    store.dispatch(deleteTodo(id));
+}
+
+const paintToDos = () =>{
+    const toDos = store.getState();
+    ul.innerHTML = "";
+    toDos.forEach(toDo =>{
+        const li = document.createElement("li");
+        const btn = document.createElement("button");
+        btn.addEventListener("click", dispatchDeleteTodo);
+        btn.innerText="delete";
+        li.id = toDo.id;
+        li.innerText = toDo.text;
+        li.appendChild(btn);
+        ul.appendChild(li);
+    })
+}
+
+store.subscribe(paintToDos);
 
 const onSubmit = e =>{
     e.preventDefault();
     const toDo = input.value;
     input.value = "";
-    store.dispatch({type : ADD_TODO, text : toDo});
+    dispatchAddTodo(toDo);
 }
 
-form.addEventListener("submit", onSubmit);
+form.addEventListener("submit", onSubmit); 
